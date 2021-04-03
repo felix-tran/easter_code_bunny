845548eb8ac9e2013146ec5c0868e34cda5b01b4
Felix Tran <felix.tr85@gmail.com>
2021-04-03 13:52:52 +0200

    fixed a bug that always print Congratulations

--- 3/phone_letters.py
+++ 3/phone_letters.py
@@ -1,6 +1,6 @@
 def phoneLetters(num):
 
-    keyMappings = {"1":"_","2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz", "0":"-"}
+    keyMappings = {"1":"_","2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz", "0":"_"}
     combinations = [""]
     temp = [""]
 
@@ -12,7 +12,7 @@ def phoneLetters(num):
         temp = [x + y for x in temp for y in keyMappings[num[i]]]
         combinations = [combo for combo in temp if len(combo) == n]
 
-    if "easter-egg" or "easter_egg" in combinations:
+    if "easter_egg" in combinations:
          print('Congratulations! You found the "Easter Egg" combination!')
     else:
          print(combinations)
